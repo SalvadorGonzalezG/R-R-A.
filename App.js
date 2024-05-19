926f02d941ee4d2e72f65f5a3fad5f30552b9d63
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-05-18 23:33:36 -0600

    added linter & probando l-5

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("wlcome")
   return (
     <View style={styles.container}>
-      <Text>Bienvenido Salvador in your app...</Text>
+      <Text>Neoker 587 Bienvenido  your app here we go.</Text>
       <Image 
       source= {{
         uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
       }}
       style={{width: 200, height:200}}
       />
+      <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
