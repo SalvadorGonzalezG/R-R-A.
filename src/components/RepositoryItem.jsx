0dffd14beff13ffb9c117bb6c4445056e93cd277
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-05-26 20:51:51 -0600

    creating the RepositoryItemHeader function which will render the content that will be displayed, this function using props calls the data that will be displayed on the screen

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,17 +1,27 @@
 import React from "react"
-import {Image, Text, View, StyleSheet } from "react-native"
+import { Image, Text, View, StyleSheet } from "react-native"
 import StyledText from "./StyleText"
 import RepositoryStats from '../components/RepositoryStats'
 import theme from "../theme"
 
+// funcion que renderiza el contenido, se crea cada una de las cards donde se manda llamar el contenido.
+const RepositoryItemHeader = ({ownerAvatarUrl,fullName,description,language}
+) => {
+    return(
+    <View>
+        <Image style={styles.image} source={{ uri: ownerAvatarUrl }} />
+        <StyledText style={styles.language}>{language}</StyledText>
+        <StyledText fontSize='subheading' fontWeight='bold'>name: {fullName}</StyledText>
+        <StyledText color='primary'>{description}</StyledText>
+        
+    </View>
+    );
+};
 
 const RepositoryItem = (props) => {
     return (
         <View key={props.id} style={styles.container}>
-            <Image style={styles.image} source={{uri: props.ownerAvatarUrl}}/>
-            <StyledText fontSize='subheading' fontWeight='bold'>name: {props.fullName}</StyledText>
-            <StyledText color='primary'>description: {props.description}</StyledText>
-            <StyledText style={styles.language}> lenguage: {props.language}</StyledText>
+            <RepositoryItemHeader {...props} />
             <RepositoryStats {...props} />
         </View>
     )
@@ -24,19 +34,19 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         paddingTop: 10
     },
-    language:{
-        padding:4,
+    language: {
+        padding: 4,
         color: theme.colors.white,
         backgroundColor: theme.colors.primary,
-        alignSelf: 'flex-end',
+        alignSelf: 'flex-start',
         borderRadius: 6,
         overflow: 'hidden'
     },
     image: {
-        width:48,
+        width: 48,
         height: 48,
-        borderRadius:8,
-        alignSelf: 'flex-end'
+        borderRadius: 8,
+        
     }
 })
 export default RepositoryItem
